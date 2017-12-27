38f041591dc0417383cb20feb1f8866e60a29c7f
roger109z <roger_109g@yahoo.com>
2017-12-26 23:29:02 -0700

    Update updater.lua

--- updater.lua
+++ updater.lua
@@ -1,9 +1,9 @@
 local files = {
-  startup = "https://github.com/roger109z/BetaBot/updater.lua",
-  startup2 = "https://github.com/roger109z/BetaBot/startup.lua",
-  command = "https://github.com/roger109z/BetaBot/commands.lua",
-  color = "https://github.com/roger109z/BetaBot/color.lua",
-  motd = "https://github.com/roger109z/BetaBot/motd.txt",
+  startup = "https://raw.githubusercontent.com/roger109z/BetaBot/updater.lua",
+  startup2 = "https://raw.githubusercontent.com/roger109z/BetaBot/startup.lua",
+  command = "https://raw.githubusercontent.com/roger109z/BetaBot/commands.lua",
+  color = "https://raw.githubusercontent.com/roger109z/BetaBot/color.lua",
+  motd = "https://raw.githubusercontent.com/roger109z/BetaBot/motd.txt",
 }
 for k, v in pairs(files) do
   local site = http.get(v)
