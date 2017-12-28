ee4598099b82f5c60033a7ee8dacc9321f18ef08
TheDacinator <dacman9@gmail.com>
2017-12-28 12:49:54 -0500

    Update commands.lua
    Added balance command as well as other misc changes

--- commands.lua
+++ commands.lua
@@ -455,8 +455,17 @@ local function main()--the main function it's only a function because I needed t
                     elseif command[2] == "balance" then
                         if not (#command == 2 or #command = 3) then
                             tell(name,badsyntax)
+                        elseif #command == 3 and (not gamma[command[3]]) then
+                            tell(name,"&6Player is not in the database")
+                        else
+                            local p
+                            if command[3] then
+                                p = command[3]
+                            else
+                                p = name
+                            end
+                            tell(name,"Balance: "..gamma[p])
                         end
-                        
                     else
                         tell(name,"&6Invalid command")
                     end
