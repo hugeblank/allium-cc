f73495c3099411ae53684833b90dbcffe20b8d47
Todd Lange <toddlangejr@gmail.com>
2017-12-31 22:56:20 -0800

    added help, and hidetag into tell

--- core.lua
+++ core.lua
@@ -1,19 +1,19 @@
-print("Loading BagelBot") --Sponsored by roger109z
-os.loadAPI("color.lua")
+print("Loading BagelBot")
+os.loadAPI("color.lua") --Sponsored by roger109z
 _G.bagelBot = {}
 local command = {}
 local threads = {}
 local thelp = {}
 local mName = "<&6Bagel&eBot>"
 print("Integrating API...")
-_G.bagelBot.tell = function(name, message)
+_G.bagelBot.tell = function(name, message, hidetag)
     local m
     if type(message) == "string" then
         m = message
     else
         m = ""
     end
-    local _, test = commands.tellraw(name, color.format(mName.."&r: "..m))
+    local _, test = commands.tellraw(name, color.format((function(hidetag)if hidetag then return "" else return mName.."&r: " end end)(hidetag)..m))
     if type(message) == "table" then
         for k, v in pairs(message) do
             local _, l = commands.tellraw(name, color.format(v))
@@ -46,8 +46,12 @@ local help = function()
 	name, args = bagelBot.out()
 	if args[1] == "help" then
 		bagelBot.tell(name, "This one is tricky to understand, so it has been omitted.")
+	elseif args[1] == nil then
+		for k, v in pairs(thelp) do
+			bagelBot.tell(name, "&6*&r &g&c!"..k.."&r: "..v, true)
+		end
 	else
-		bagelBot.tell(name, thelp[args])
+		bagelBot.tell(name, thelp[args[1]])
 	end
 end
 commands["help"] = help
@@ -55,6 +59,7 @@ local main = function()
 	while true do
 		local _, _, name, message = os.pullEvent("chat_message")
 		if string.find(message, "!") == 1 then
+			command = {}
 			for k in string.gmatch(message, "%S+") do
 				command[#command+1] = k
 			end
@@ -63,7 +68,7 @@ local main = function()
 			if commands[cmd] ~= nil then
 				_G.bagelBot.out = function() return name, command end
 	    		local _, out = commands[cmd](table.concat(command)) --the parameter here is only for vanilla command implementation. Use bagelBot.out if you want access to the arguments (they come in a nice table too).
-    			if out then bagelBot.tell(name, out[1]) end
+    			if out then bagelBot.tell(name, out[1].." beep") end
     		else
 	    		bagelBot.tell(name, "&6Invalid Command, use &c&g!help&r&6 for assistance.")
     		end
@@ -80,18 +85,35 @@ local living = count
 local tFilters = {}
 local eventData = { n = 0 }
 while true do
-    for n=1,count do
-        local r = threads[n]
-        if r then
-            if tFilters[r] == nil or tFilters[r] == eventData[1] or eventData[1] == "terminate" then
-                local ok, param = coroutine.resume( r, table.unpack( eventData, 1, eventData.n ) )
-                if not ok then
-                    error( param, 0 )
-                else
-                    tFilters[r] = param
-                end
-            end
-        end
-    end
-    eventData = table.pack( os.pullEventRaw() )
+	for n=1,count do
+		local r = threads[n]
+		if r then
+			if tFilters[r] == nil or tFilters[r] == eventData[1] or eventData[1] == "terminate" then
+    			local ok, param = coroutine.resume( r, table.unpack( eventData, 1, eventData.n ) )
+				if not ok then
+					error( param, 0 )
+				else
+					tFilters[r] = param
+				end
+				if coroutine.status( r ) == "dead" then
+					threads[n] = nil
+					living = living - 1
+					if living <= 0 then
+						return n
+					end
+				end
+			end
+		end
+	end
+	for n=1,count do
+		local r = threads[n]
+		if r and coroutine.status( r ) == "dead" then
+			threads[n] = nil
+			living = living - 1
+			if living <= 0 then
+				return n
+			end
+		end
+	end
+	eventData = table.pack( os.pullEventRaw() )
 end
