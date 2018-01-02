b654d1eff957b843e357c845b0efc04127ad122a
Todd Lange <toddlangejr@gmail.com>
2018-01-01 23:13:17 -0800

    - Moved plugin command table out from commands so that cheatable commands aren't accessible to any old user. - Added !plugins, so that all titles of plugins can be viewable. - Commented the hell out of core.lua. - Added help text entries for the core implemented commands (help, github, and plugins). - Fixed BagelBots chat name.

--- core.lua
+++ core.lua
@@ -1,20 +1,22 @@
 print("Loading BagelBot")
 os.loadAPI("color.lua") --Sponsored by roger109z
 _G.bagelBot = {}
+local botcmds = {}
+local pluginlist = {}
 local command = {}
 local threads = {}
 local thelp = {}
 local dir = shell.dir()
-local mName = "<&6Bagel&eBot>" --bot title
+local mName = "<&6Bagel&eBot&f>" --bot title
 print("Integrating API...")
-_G.bagelBot.tell = function(name, message, hidetag)
+_G.bagelBot.tell = function(name, message, hidetag) --bagelBot.tell as documented in README
     local m
     if type(message) == "string" then
         m = message
     else
         m = ""
     end
-    local _, test = commands.tellraw(name, color.format((function(hidetag)if hidetag then return "" else return mName.."&r: " end end)(hidetag)..m))
+    local _, test = commands.tellraw(name, color.format((function(hidetag)if hidetag then return "" else return mName.."&r" end end)(hidetag)..m))
     if type(message) == "table" then
         for k, v in pairs(message) do
             local _, l = commands.tellraw(name, color.format(v))
@@ -22,7 +24,7 @@ _G.bagelBot.tell = function(name, message, hidetag)
     end
     return textutils.serialise(test)
 end
-_G.bagelBot.getPersistence = function(name)
+_G.bagelBot.getPersistence = function(name) --bagelBot.getPersistence as documented in README
 	if fs.exists("persistence.json") then
 		local fper = fs.open("persistence.json", "r")
 		local tpersist = textutils.unserialize(fper.readAll())
@@ -32,7 +34,7 @@ _G.bagelBot.getPersistence = function(name)
 		return false
 	end
 end
-_G.bagelBot.setPersistence = function(name, data)
+_G.bagelBot.setPersistence = function(name, data) --bagelBot.setPersistence as documented in README
 	local tpersist
 	if fs.exists("persistence.json") then
 		local fper = fs.open("persistence.json", "r")
@@ -46,18 +48,19 @@ _G.bagelBot.setPersistence = function(name, data)
 end
 print("Loading plugins...")
 for _, plugin in pairs(fs.list(dir.."plugins")) do
+	pluginlist[#pluginlist+1] = plugin
 	if fs.isDir(dir.."plugins/"..plugin) then
-		if fs.exists(dir.."plugins/"..plugin.."/init.lua") then
+		if fs.exists(dir.."plugins/"..plugin.."/init.lua") then --load init.lua
 			shell.run(dir.."plugins/"..plugin.."/init.lua")
 		end
-		if fs.isDir(dir.."plugins/"..plugin.."/threads") then
+		if fs.isDir(dir.."plugins/"..plugin.."/threads") then --load threads
 			for _, v in pairs(fs.list(dir.."plugins/"..plugin.."/threads")) do
 				threads[#threads+1] = coroutine.create(loadfile(dir.."plugins/"..plugin.."/threads/"..v))
 			end
 		end
-		for _, v in pairs(fs.list(dir.."plugins/"..plugin.."/commands")) do
+		for _, v in pairs(fs.list(dir.."plugins/"..plugin.."/commands")) do --load commands & help entries
 			local name = v:sub(1, -5)
-			_G.commands[name] = loadfile(dir.."plugins/"..plugin.."/commands/"..v)
+			_G.botcmds[name] = loadfile(dir.."plugins/"..plugin.."/commands/"..v)
 			if fs.exists(dir.."plugins/"..plugin.."/help/"..name..".txt") then
 				local txt = fs.open(dir.."plugins/"..plugin.."/help/"..name..".txt", "r")
 				thelp[name] = txt.readAll()
@@ -69,47 +72,62 @@ for _, plugin in pairs(fs.list(dir.."plugins")) do
 	end
 end
 print("Integrating core components...")
-local help = function()
+local help = function() --!help integration
 	name, args = bagelBot.out()
-	if args[1] == "help" then
-		bagelBot.tell(name, "&cError Parameter 1: Attempt to call Cheeky Bastard")
 	elseif args[1] == nil then
 		for k, v in pairs(thelp) do
-			bagelBot.tell(name, "&6*&r &c&g!"..k.."&r: "..v, true)
+			bagelBot.tell(name, "&6*&r &g&c!"..k.."&r: "..v, true)
 		end
 	else
 		bagelBot.tell(name, thelp[args[1]])
 	end
 end
-local github = function()
+local github = function() --!github integration
 	name, args = bagelBot.out()
 	bagelBot.tell(name, "Contribute to bagelBot here: https://github.com/hugeblank/BagelBot")
 end
-commands["help"] = help
-commands["github"] = github
+local plugins = function() --!plugins integration
+	name = bagelBot.out()
+	local str = ""
+	for i = 1, #pluginlist do
+		if i < #pluginlist then
+			str = str.."&a"..pluginlist[i].."&f, "
+		else
+			str = str.."&a"..pluginlist[i]
+		end
+	end
+	bagelBot.tell(name, "\nPlugins installed: "..str)
+end
+--adding commands and integrating help entries for them
+botcmds["help"] = help
+botcmds["github"] = github
+botcmds["plugins"] = plugins
+thelp["github"] = "Provides the github repo to check out"
+thelp["plugins"] = "Lists the name of all plugins installed on the bot"
+thelp["help"] = "Provides help for help for help for help for help for help"
+
 local main = function()
 	while true do
-		local _, _, name, message = os.pullEvent("chat_message")
-		if string.find(message, "!") == 1 then
+		local _, _, name, message = os.pullEvent("chat_message") --Pull chat messages
+		if string.find(message, "!") == 1 then --are they for BagelBot?
 			command = {}
-			for k in string.gmatch(message, "%S+") do
+			for k in string.gmatch(message, "%S+") do --put all arguments spaced out into a table
 				command[#command+1] = k
 			end
 			local cmd = string.sub(command[1], 2)
-			table.remove(command, 1)
-			if commands[cmd] ~= nil then
-				_G.bagelBot.out = function() return name, command end
-	    		local _, out = commands[cmd](table.concat(command)) --the parameter here is only for vanilla command implementation. Use bagelBot.out if you want access to the arguments (they come in a nice table too).
-    			if out then bagelBot.tell(name, out[1].." beep") end
-    		else
-	    		bagelBot.tell(name, "&6Invalid Command, use &c&g!help&r&6 for assistance.")
+			table.remove(command, 1) --remove the first parameter given (!command)
+			if botcmds[cmd] ~= nil then --is it really a command?
+				_G.bagelBot.out = function() return name, command end --bagelBot.out as documented in README
+	    		botcmds[cmd]() --Let's execute the command
+    		else --this isn't a valid command...
+	    		bagelBot.tell(name, "&6Invalid Command, use &c&g!help&r&6 for assistance.") --bleh!
     		end
 	    end
 	end
 end
-threads[#threads+1] = coroutine.create(main)
+threads[#threads+1] = coroutine.create(main) --Add main to the thread table
 
-if not fs.exists("persistence.json") then
+if not fs.exists("persistence.json") then --In the situation that this is a first installation, let's add persistence.json
 	local fpers = fs.open("persistence.json", "w")
 	fpers.write("{}")
 	fpers.close()
@@ -117,6 +135,7 @@ end
 
 print("BagelBot started.")
 bagelBot.tell("@a", "BagelBot loaded.")
+--This clump is pulled and adapted for what I need it for, parallel.waitForAll, for a table of coroutines. Its origin is in /rom/apis/parallel.lua in CraftOS.
 local count = #threads
 local living = count
 
