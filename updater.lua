8fed616e792c624112b5b7336aa15a1e8c24b5a8
hugeblank <toddlangejr@gmail.com>
2018-01-01 20:22:23 -0800

    Update updater.lua

--- updater.lua
+++ updater.lua
@@ -1,4 +1,4 @@
-local files = { "https://raw.githubusercontent.com/hugeblank/BagelBot/master/updater.lua", "https://raw.githubusercontent.com/hugeblank/BagelBot/master/startup.lua", "https://raw.githubusercontent.com/hugeblank/BagelBot/master/core.lua", "https://raw.githubusercontent.com/hugeblank/BagelBot/master/color.lua", "https://raw.githubusercontent.com/hugeblank/BagelBot/master/motd.txt"}
+local files = { "https://raw.githubusercontent.com/hugeblank/BagelBot/master/updater.lua", "https://raw.githubusercontent.com/hugeblank/BagelBot/master/core.lua", "https://raw.githubusercontent.com/hugeblank/BagelBot/master/color.lua", "https://raw.githubusercontent.com/hugeblank/BagelBot/master/motd.txt"}
 local getName = function(index) local t = {} local name for i in string.gmatch(files[index], "[^/]+") do t[#t+1] = i end return t[#t] end
 for i = 1, #files do shell.run("rm "..getName(i)) shell.run("wget "..files[i], getName(i)) end
 shell.run("mkdir plugins")
