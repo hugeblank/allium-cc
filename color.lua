150842ca927a5c3ae67d309610c513d4487d6be8
roger109z <roger_109g@yahoo.com>
2018-01-03 23:10:53 -0800

    Update color.lua
    Major Color.lua update
    added:
        l = "bold"
        n = "underline"
        o = "italic"
        k = "obfuscated"
        m = "strikethrough"
        s = "suggest_command"
        i = "link" 
        h = "hoverEvent"
    changed:
        when using an action command you now have to clear it with &r this allows for more flexibility

--- color.lua
+++ color.lua
@@ -1,5 +1,4 @@
 local cTable = {
-    r = "white",
     ["0"] = "black",
     ["1"] = "dark_blue",
     ["2"] = "dark_green",
@@ -27,14 +26,32 @@ local formats = {
 local actions = {
     s = "suggest_command",
     g = "run_command" ,
-    h = "link" ,
+    i = "link" ,
 }
 local other = {
-    i = "hovertext",
+    h = "hoverEvent",
     r = "reset",
 }
-format = function(sText)
-    local current = {}
+local dCurrent = {
+	format = {
+    	bold = false,
+    	underline = false,
+    	italic = false,
+    	obfuscated = false,
+    	strikethrough = false,
+	end
+    color = "white",
+    hoverEvent = false,
+    action = false
+	actionText = ""
+	hoverText = ""
+}
+    
+format = function(sText, bAction)
+	if type(bAction) ~= "boolean" then
+		bAction = true
+	end
+    local current = dCurrent
     local seperated = {}
     sText = "&r"..sText
     for k in string.gmatch(sText, "[^&]+") do
@@ -42,16 +59,61 @@ format = function(sText)
     end
     local outText = '["",'
     local prev
+	local function escape(tbl)
+		for k, v in pairs(tbl) do
+			if v[2]:find("\\") == v[2]:len() then
+				tbl[k] = {v[1], v[2]:sub(1, -1).."&"..tbl[tonumber(k)+1][1]..tbl[tonumber(k)+1][2]}
+				table.remove(tbl, tonumer(k)+1)
+				return escape(tbl)
+			end
+		end
+	end
+	seperated = escape(seperated)
     for k, v in pairs(seperated) do
-        local color = cTable[v[1]] or prev 
-        outText = outText..'{"text":"'..v[2]..'","color":"'..color..'"'
-        if v[1] == "g" then
-            local ind = string.find(v[2], ")")
-            local action = string.sub(v[2], 2, ind-1)
-            outText = string.sub(outText, 1, -(22+string.len(v[2])+string.len(color)))
-            outText = outText..'{"text":"'..string.sub(v[2], ind+1)..'","color":"'..color..'"'
-            outText = outText..',"clickEvent":{"action":"run_command","value":"'..action..'"}'
-        end
+        if cTable[v[1]] ~= nil then
+            current["color"] = cTable[v[1]]
+        elseif formats[v[1]] ~= nil then 
+			if current["format"][formats[v[1]]] = false then
+				current["format"][formats[v[1]]] = true
+		    else
+				current["format"][formats[v[1]]] = false
+			end
+		elseif actions[v[1]] ~= nil then
+			current["action"] = actions[v[1]]
+			local ind = string.find(v[2], ")")
+			if ind ~= nil then
+            	current["actionText"] = string.sub(v[2], 2, ind-1)
+				v[2] = v[2]:sub(1, ind)
+			else
+				current["actionText"] = v[2]
+			end
+		elseif other[v[1]] ~= nil then
+			if other[v[1]] == "hoverEvent" then
+				current["hoverEvent"] = true
+				local ind = string.find(v[2], ")")
+				if ind ~= nil then
+					current["hoverText"] = format(string.sub(v[2], 2, ind-1), false)
+					v[2] = v[2]:sub(1, ind)
+				else
+					current["hoverText"] = v[2]
+				end
+			elseif actions[v[1]] == "reset" then
+				current = dCurrent
+			end
+		else
+			v[2] = "&"..v[1]..v[2]
+		end
+        outText = outText..'{"text":"'..v[2]..'","color":"'..current["color"]..'"'
+		for k, v in pairs(current["format"])
+			if v then
+				outText = outText..",\""..k.."\":true"
+			end
+		end
+		if current["action"] ~= false and bAction then
+			outText = outText..',"clickEvent":{"action":"'..current["action"]..'","value":"'..current["actionText"]..'"}'
+		end
+		if current["hoverEvent"] ~= false and bAction then
+			outText = outText..',"hoverEvent":{"action":"show_text","value":"'..current["hoverText"]..'"}'
         outText = outText..'},'
         prev = color
     end
