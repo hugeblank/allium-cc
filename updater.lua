07de4641544ce0d8f44ae369ae23887049f5e75d
roger109z <roger_109g@yahoo.com>
2018-01-03 20:20:20 -0800

    Update updater.lua

--- updater.lua
+++ updater.lua
@@ -1,10 +1,14 @@
 shell.run("fg")
-local file = fs.open(".repoList", "r")
-if file ~= nil then
+if fs.exists("/.repoList") then
+    local file = fs.open("/.repoList", "r")
     for k in file.readLine do
         shell.run("github clone "..k.." /")
     end
+    file.close()
 else
-    error("No valid repo file!")
+    local file = fs.open("/.repoList", "w")
+    file.write("hugeblank/BagelBot")
+    file.close()
+    error("No valid repo file! Default repo file created!")
 end
 shell.run("core.lua")
