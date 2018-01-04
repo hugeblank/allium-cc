c90a39730033f6685f00d5d6a44ee4625b10dcf7
Todd Lange <toddlangejr@gmail.com>
2018-01-03 20:48:42 -0800

    Added file location to repoList data

--- startup.lua
+++ startup.lua
@@ -2,12 +2,12 @@ shell.run("fg")
 if fs.exists("/.repoList") then
     local file = fs.open("/.repoList", "r")
     for k in file.readLine do
-        shell.run("github clone "..k.." /")
+        shell.run("github clone "..k)
     end
     file.close()
 else
     local file = fs.open("/.repoList", "w")
-    file.write("hugeblank/BagelBot") --Forkers change this to their repository.
+    file.write("hugeblank/BagelBot /") --Forkers change this to their repository.
     file.close()
     error("No valid repo file! Default repo file created!")
 end
