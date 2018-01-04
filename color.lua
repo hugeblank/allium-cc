ad2c37b2a68507ad621f51b52147663e5a93dabd
Todd Lange <toddlangejr@gmail.com>
2018-01-04 01:11:40 -0800

    Updated color.lua to latest version

--- color.lua
+++ color.lua
@@ -1,23 +1,78 @@
 local cTable = {
-r = "white",
-["0"] = "black",
-["1"] = "dark_blue",
-["2"] = "dark_green",
-["3"] = "dark_aqua",
-["4"] = "dark_red",
-["5"] = "dark_purple",
-["6"] = "gold",
-["7"] = "gray",
-["8"] = "dark_gray",
-["9"] = "blue",
-a = "green",
-b = "aqua",
-c = "red",
-d = "light_purple",
-e = "yellow",
-f = "white"
+    ["0"] = "black",
+    ["1"] = "dark_blue",
+    ["2"] = "dark_green",
+    ["3"] = "dark_aqua",
+    ["4"] = "dark_red",
+    ["5"] = "dark_purple",
+    ["6"] = "gold",
+    ["7"] = "gray",
+    ["8"] = "dark_gray",
+    ["9"] = "blue",
+    a = "green",
+    b = "aqua",
+    c = "red",
+    d = "light_purple",
+    e = "yellow",
+    f = "white"
 }
-format = function(sText)
+local formats = {
+    l = "bold",
+    n = "underlined",
+    o = "italic",
+    k = "obfuscated",
+    m = "strikethrough",
+}
+local actions = {
+    s = "suggest_command",
+    g = "run_command" ,
+    i = "open_url" ,
+}
+local other = {
+    h = "hoverEvent",
+    r = "reset",
+}
+local dCurrent = {
+    format = {
+        bold = false,
+        underlined = false,
+        italic = false,
+        obfuscated = false,
+        strikethrough = false,
+    },
+    color = "white",
+    hoverEvent = false,
+    action = false,
+    actionText = "",
+    hoverText = "",
+}
+local function escape(tbl)
+    for k, v in pairs(tbl) do
+        if v[2]:find("\\") == v[2]:len() then
+            tbl[k] = {v[1], v[2]:sub(1, -1).."&"..tbl[tonumber(k)+1][1]..tbl[tonumber(k)+1][2]}
+            table.remove(tbl, tonumer(k)+1)
+            local ret = escape(tbl)
+            return ret
+        end
+    end
+    return tbl
+end
+local function copy(tbl)
+    local ret = {}
+    for k, v in pairs(tbl) do
+        if type(v) ~= "table" then
+            ret[k] = v
+        else
+            ret[k] = copy(v)
+        end
+    end
+    return ret
+end
+format = function(sText, bAction)
+    if type(bAction) ~= "boolean" then
+        bAction = true
+    end
+    local current = copy(dCurrent)
     local seperated = {}
     sText = "&r"..sText
     for k in string.gmatch(sText, "[^&]+") do
@@ -25,19 +80,68 @@ format = function(sText)
     end
     local outText = '["",'
     local prev
+    seperated = escape(seperated)
     for k, v in pairs(seperated) do
-        local color = cTable[v[1]] or prev 
-        outText = outText..'{"text":"'..v[2]..'","color":"'..color..'"'
-        if v[1] == "g" then
+        if cTable[v[1]] ~= nil then
+            current["color"] = cTable[v[1]]
+        elseif formats[v[1]] ~= nil then 
+            if current["format"][formats[v[1]]] == false then
+                current["format"][formats[v[1]]] = true
+            else
+                current["format"][formats[v[1]]] = false
+            end
+        elseif actions[v[1]] ~= nil then
+            current["action"] = actions[v[1]]
             local ind = string.find(v[2], ")")
-            local action = string.sub(v[2], 2, ind-1)
-            outText = string.sub(outText, 1, -(22+string.len(v[2])+string.len(color)))
-            outText = outText..'{"text":"'..string.sub(v[2], ind+1)..'","color":"'..color..'"'
-            outText = outText..',"clickEvent":{"action":"run_command","value":"'..action..'"}'
+            if ind ~= nil then
+                current["actionText"] = string.sub(v[2], 2, ind-1)
+                v[2] = v[2]:sub(ind+1)
+            else
+                current["actionText"] = v[2]
+            end
+        elseif other[v[1]] ~= nil then
+            if other[v[1]] == "hoverEvent" then
+                current["hoverEvent"] = true
+                local ind = string.find(v[2], ")")
+                if ind ~= nil then
+                    current["hoverText"] = format(string.sub(v[2], 2, ind-1), false)
+                    v[2] = v[2]:sub(ind+1)
+                else
+                    current["hoverText"] = v[2]
+                end
+            elseif other[v[1]] == "reset" then
+                current = copy(dCurrent)
+            end
+        else
+            v[2] = "&"..v[1]..v[2]
+        end
+        outText = outText..'{"text":"'..v[2]..'","color":"'..current["color"]..'"'
+        for k, v in pairs(current["format"]) do
+            if v then
+                outText = outText..",\""..k.."\":true"
+            end
+        end
+        if current["action"] ~= false and bAction then
+            outText = outText..',"clickEvent":{"action":"'..current["action"]..'","value":"'..current["actionText"]..'"}'
+        end
+        if current["hoverEvent"] ~= false and bAction then
+            outText = outText..',"hoverEvent":{"action":"show_text","value":'..current["hoverText"]..'}'
         end
         outText = outText..'},'
-        prev = color
     end
     outText = string.sub(outText, 1, -2)..']'
     return outText
+end
+
+deformat = function(string)
+    local seperated = {}
+    local out = ""
+    for k in string.gmatch(sText, "[^&]+") do
+        seperated[#seperated+1] = {string.sub(k, 1, 1), string.sub(k, 2)}
+    end
+    seperated = escape(seperated)
+    for k, v in pairs(seperated) do
+        out = out..v
+    end
+    return out
 end
