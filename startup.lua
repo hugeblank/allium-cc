390e6272ecdcb419e69aa33b9cb5118df6b5dd17
roger109z <roger@DESKTOP-5SIK452.localdomain>
2019-10-16 00:00:20 -0700

    Slimmed down startup.lua

--- startup.lua
+++ startup.lua
@@ -8,65 +8,26 @@ if not commands then -- Attempt to prevent user from running this on non-command
 	return
 end
 
-local config = {}
-
-do -- Configuration parsing
-	local file, default = fs.open("cfg/allium.lson", "r"), {updates = {deps = true, allium = true}}
-	local function verify_cfg(input, default, index)
-		for f_k, f_v in pairs(input) do -- input key, value
-			for t_k, t_v in pairs(default) do -- standard key, value
-				if type(f_v) == "table" and type(t_v) == "table" then
-					if not verify_cfg(f_v, t_v, f_k..".") then
-						return false
-					end
-				elseif f_k == t_k and type(f_v) ~= type(t_v) then
-					printError("Invalid config option "..(index or "")..f_k.." (expected "..type(t_v)..", got "..type(f_v)..")")
-					return false
-				end
-			end
-		end
-		return true
-	end
-	local function fill_missing(file, default)
-        local out = {}
-        if not file then file = {} end
-        for k, v in pairs(default) do
-            if type(v) == "table" then
-                out[k] = fill_missing(file[k], v)
-            else
-                if type(file[k]) == "nil" then
-                    out[k] = v
-                else
-                    out[k] = file[k]
-                end
-            end
-        end
-        for k, v in pairs(file) do
-            if out[k] == nil then
-                out[k] = v
-            end
-        end
-        return out
+--load settings from file
+local loadSettings = function(file, default)
+    assert(type(file) == "string", "file must be a string")  
+    if not fs.exists(file) then
+        local setting = fs.open(file,"w")
+        setting.write(textutils.serialise(default))
+        setting.close()
+        return default
     end
-    local output = {}
-	if file then -- Could not read file
-        output = textutils.unserialise(file.readAll()) or {}
-        file.close()
-	end
-	if verify_cfg(output, default) then -- Make sure none of the config opts are invalid (skips missing ones)
-        config = fill_missing(output, default) -- Fill in the remaining options that are missing
-        if config.version ~= allium_version then
-            config.version = allium_version
-            file = fs.open("cfg/allium.lson", "w")
-            if file then
-                file.write(textutils.serialise(config))
-                file.close()
-            end
-        end
-	else
-		return
-	end
+    local setting = fs.open(file, "r")
+    local config = setting.readAll()
+    setting.close()
+    config = textutils.unserialise(config)
+    if type(config) ~= "table" then
+        return default
+    end
+    return config
 end
+
+config = loadSettings("cfg/allium.lson", {updates = {dependencies = true, allium = true}})
 
 -- Checking Allium/Plugin updates
 if config.updates.allium then
@@ -79,7 +40,7 @@ if config.updates.allium then
 end
 
 -- Filling Dependencies
-if config.updates.deps then
+if config.updates.dependencies then
     -- Allium DepMan Instance: https://pastebin.com/nRgBd3b6
     print("Updating Dependencies...")
     local didrun = false
